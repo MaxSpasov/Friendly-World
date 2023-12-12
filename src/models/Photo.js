68da5c14c37ddf9ed10603e693ca191f682e0677
MaxSpasov <114147361+MaxSpasov@users.noreply.github.com>
2023-12-12 10:22:42 +0200

    Changes

--- src/models/Photo.js
+++ src/models/Photo.js
@@ -5,29 +5,29 @@ const photoSchema = new mongoose.Schema({
         type: String,
         required: [true, 'Name is required'],
     },
-    image: {
-        type: String,
-        required: [true, 'ImageUrl is required'],
-    },
     age: {
         type: Number,
         required: [true, 'Age is required'],
-    },
-    description: {
-        type: String,
-        required: [true, 'Description is required'],
     },
     kind: {
         type: String,
         required: [true, 'Kind is required'],
     },
-    location: {
+    image: {
         type: String,
-        required: [true, 'Location is required'],
+        required: [true, 'ImageUrl is required'],
     },
     needsOf: {
         type: String,
         required: [true, 'Needs are required']
+    },
+    location: {
+        type: String,
+        required: [true, 'Location is required'],
+    },
+    description: {
+        type: String,
+        required: [true, 'Description is required'],
     },
     owner: {
         type: mongoose.Types.ObjectId,
